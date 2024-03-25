34070cec44ce51adeb389cd14da324c28ee72067
Matias Zuber <107443698+MattZubSch@users.noreply.github.com>
2024-03-25 04:25:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header.js';
 import MainPage from './pages/main.js';
 import RenderMatch from './components/renderMatches.js';
-import MatchRender from './components/MatchRender'
+import MatchRender from './components/MatchRender.js'
 import SimulatorScreen from './pages/simulatorScreen.js';
 
 function App() {
